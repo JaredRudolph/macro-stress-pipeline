2ee46e5a0d0d4dcbcd8f5c21598489deaee6a5e2
Jared Rudolph <jkrudolph@outlook.com>
2026-03-13 04:14:03 -0400

    Remove unused pandas import and simplify run function call in main script

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import pandas as pd
 from dotenv import load_dotenv
+
 from macro_stress.pipeline import run
 
 load_dotenv()
 
 if __name__ == "__main__":
-    end = pd.Timestamp.today().strftime("%Y-%m-%d")
-    run(start="2020-01-01", end=end)
+    run()
